944f4973d1b5c1392d1ca63c0ccbc1b22c298603
Tanguy Maurin <tanguy.maurin@outlook.com>
2026-03-16 22:29:02 +0100

    Chapter 2 - data structures and control flow

---
+++ chapter2.py
@@ -0,0 +1,10 @@
+commodity={
+    "name":"Gold",
+    "price":1923.4,
+    "currency":"USD",
+    "unit":"ounce"
+}
+
+print(commodity["name"])
+print(commodity["price"])
+print(f"{commodity["name"]} is trading at {commodity["price"]} {commodity["currency"]} per {commodity["unit"]}  ")
