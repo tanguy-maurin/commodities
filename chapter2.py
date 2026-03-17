7fdfbd87cf54ca4fcbe75c0306220812f411c2a7
tanguy-maurin <tanguy.maurin@outlook.com>
2026-03-17 09:31:28 +0000

    Chapter 2 - data structures and control flow

--- chapter2.py
+++ chapter2.py
@@ -1,10 +1,28 @@
-commodity={
-    "name":"Gold",
-    "price":1923.4,
-    "currency":"USD",
-    "unit":"ounce"
-}
 
-print(commodity["name"])
-print(commodity["price"])
-print(f"{commodity["name"]} is trading at {commodity["price"]} {commodity["currency"]} per {commodity["unit"]}  ")
+commodities = [
+    {"name": "WTI", "price":87.3, "signal":None},
+    {"name" : "Brent", "price":103.6, "signal":None}
+]
+
+for c_i in commodities :
+    if c_i["price"] > 100 :
+        c_i["signal"] = "SELL"
+    elif c_i["price"] < 50 :
+        c_i["signal"] = "BUY"
+    else :
+        c_i["signal"] = "HOLD"
+    print(f"{c_i["name"]} : {c_i["signal"]}")
+
+data = [
+    {"name": "Gold", "price": 1923.4},
+    {"name": "Silver", "price": 24.3},
+    {"name": "Copper", "price": 8.7}
+]
+
+result = []
+
+for item in data:
+    if item["price"] < 100:
+        result.append(item["name"])
+
+print(result)
